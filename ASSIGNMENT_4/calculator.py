5a9cd052bd41e0d8d27f3d6f7cb5395330151a2f
Jochebed-xo <jochebeds17@gmail.com>
2026-08-10 09:30:53 +0200

    Add ASSIGNMENT_4

---
+++ ASSIGNMENT_4/calculator.py
@@ -0,0 +1,18 @@
+first_num = float(input("Enter the first number"))
+second_num = float(input("Enter the second number"))
+
+addition = first_num + second_num
+subtraction = first_num - second_num
+multiplication = first_num * second_num
+division = first_num / second_num
+modulus = first_num % second_num
+power = first_num ** second_num
+
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
+print("Modulus =", modulus)
+print("Power =", power)
+
+
